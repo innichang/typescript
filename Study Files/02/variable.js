090b73704cc33529f40456675daca9681df622fc
Inseok Chang <inni.chang@gmail.com>
2023-05-21 12:33:23 +0900

    study_02

--- Study Files/02/variable.js
+++ Study Files/02/variable.js
@@ -1,2 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Inseok";
+greetings.toLowerCase();
 console.log(greetings);
+//number
+var userId = 334455;
+// userId.   //shows all the methods regarding number
+userId.toFixed();
+//userId = "Inseok"  cannot be declared since it's a string
+//boolean
+var isLoggedIn = false;
--- Study Files/02/variable.ts
+++ Study Files/02/variable.ts
@@ -1,5 +1,18 @@
 let greetings: string = "Hello Inseok";
 
+greetings.toLowerCase();
 console.log(greetings);
+
+//number
+
+let userId: number = 334455;
+
+// userId.   //shows all the methods regarding number
+userId.toFixed();
+//userId = "Inseok"  cannot be declared since it's a string
+//boolean
+let isLoggedIn: boolean = false;
+
+//isLoggedIn. //shows all the methods regarding boolean
 
 export {};
